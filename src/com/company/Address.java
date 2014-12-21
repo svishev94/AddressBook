8f838a177f9bba385e01eb1f16e2b9f0bc746d84
svishev94 <svishev7@mail.ru>
2014-12-21 23:55:13 +0400

    Update and rename Display.java to Address.java

---
+++ src/com/company/Address.java
@@ -0,0 +1,12 @@
+public class Address { //вспомогательный код
+    public String name;
+    public String phone;
+    public String email;
+    public String year;
+
+    public Address(String name, String phone, String year) {
+        this.name = name;
+        this.phone = phone;
+        this.year = year;
+    }
+}
